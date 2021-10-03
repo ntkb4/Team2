799bed2092b7c48b32eb20dce4b2ac8b59fe86ba
ntkb4 <91574282+ntkb4@users.noreply.github.com>
2021-10-03 12:26:01 -0400

    Update Team2Demo.java
    :)

--- src/team2/Team2Demo.java
+++ src/team2/Team2Demo.java
@@ -5,6 +5,7 @@ public class Team2Demo {
 	public static void main(String[] args) {
 		System.out.println("TEam2Demo");
 		System.out.println("Hello world");
+		System.out.println("More Trap...");
 		
 		
 		System.out.println("eam2Demo");
